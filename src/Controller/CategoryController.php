6ff96058eadb09c7c5d86fba4f4abae16200dfe5
Jovanela Daribo <carly972@live.fr>
2018-11-11 22:26:17 +0100

    Quete 4 MVC

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,20 +7,34 @@
  */
 
 namespace Controller;
+
 use Model\CategoryManager;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
+
 
 class CategoryController
 {
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__.'/../View');
+        $this->twig = new Twig_Environment($loader);
+    }
+
     public function index()
     {
         $categoryManager = new CategoryManager();
         $categories = $categoryManager->selectAllCategories();
-        require __DIR__ . '/../View/category.php';
+
+        return $this->twig->render('Category/category.html.twig', ['categories' => $categories]);
     }
     public function show(int $id)
     {
         $categoryManager = new CategoryManager();
         $category = $categoryManager->selectOneCategory($id);
-        require __DIR__ . '/../View/showCategory.php';
+
+        return $this->twig->render('Category/showCategory.html.twig', ['category' => $category]);
     }
 }
